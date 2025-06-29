e5dd68881df6cc211dcd801c1e1233d16b41f66e
JuanPalbo <juan.cerizola@correo.ucu.edu.uy>
2025-06-29 16:12:09 -0300

    ammend commit

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { registerLicense } from '@syncfusion/ej2-base';
+import { environment } from './environments/environment';
 
-registerLicense('ORg4AjUWIQA/Gnt2XFhhQlJHfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTH5VdEViXH5YcXxTRGBdWkZ/');
+registerLicense(environment.syncfusionKey);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
