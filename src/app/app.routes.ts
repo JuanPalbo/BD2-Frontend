459d34c2f5fee179ec02b7c0ca090e501dac99c7
florencia2003 <110925431+florencia2003@users.noreply.github.com>
2025-07-06 19:21:48 -0300

    header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Votar } from './pages/votar/votar';
 import { AbrirCircuito } from './pages/abrir-circuito/abrir-circuito';
 import { Home } from './pages/home/home';
 import { CircuitoCerrado } from './pages/circuito-cerrado/circuito-cerrado';
+import { Resultados } from './resultados/resultados';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -14,4 +15,6 @@ export const routes: Routes = [
   { path: 'abrir-circuito', component: AbrirCircuito },
   { path: 'home', component: Home },
   { path: 'circuito-cerrado', component: CircuitoCerrado },
+  { path: 'resultados-circuito', component: Resultados },
+  { path: 'resultados-globales', component: Resultados },
 ];
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
+  constructor(private router: Router) {}
 
+  irARegistrarVotante() {
+    this.router.navigate(['/registro-votante']);
+  }
+
+  cerrarCircuito() {
+    if (confirm('¿Está seguro que desea cerrar el circuito de votación?')) {
+      this.router.navigate(['/circuito-cerrado']);
+    }
+  }
+
+  logout() {
+    if (confirm('¿Está seguro que desea salir del sistema?')) {
+      this.router.navigate(['/login']);
+    }
+  }
 }
