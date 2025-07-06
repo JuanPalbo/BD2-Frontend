e17f82f3338cbe75e19c7b5e523efe9728ef351f
florencia2003 <110925431+florencia2003@users.noreply.github.com>
2025-07-06 19:27:04 -0300

    home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { Header } from '../../header/header';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, Header],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
